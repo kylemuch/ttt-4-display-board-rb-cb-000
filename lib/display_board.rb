8d9c1c9569f72886ac22d3923824923d6ab0a29f
Kyle <kylemuch@gmail.com>
2019-05-23 02:32:54 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -7,9 +7,9 @@ def display_board(arr)
   
   puts " #{arr[0]} | #{arr[1]} | #{arr[2]}  "
   puts "-----------"
-  puts "   |   |   "
+  puts " #{arr[3]} | #{arr[4]} | #{arr[5]}  "
   puts "-----------"
-  puts "   |   |   "
+  puts " #{arr[6]} | #{arr[7]} | #{arr[8]}  "
 end
 
 display_board(board_array)
